6564b6fd26a42cb5734eb4893b3a154dc5093516
Meithal <hurin8888@gmail.com>
2022-02-08 12:07:21 +0100

    pour que le login fonctionne après php 7.9

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -28,6 +28,13 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
         $this->urlGenerator = $urlGenerator;
     }
 
+    public function supports(Request $request) : bool
+    {
+        return self::LOGIN_ROUTE === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+
     public function authenticate(Request $request): Passport
     {
         $username = $request->request->get('username', '');
