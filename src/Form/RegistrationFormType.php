8a0914c31600c77d58ee598ad4af8f2dece73173
LULU <LULU@192.168.1.48>
2022-02-08 18:03:55 +0100

    Ajouter choix campus sur le formulaire d'inscription (pour régler l'erreur campus_id ne peut pas être null)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,9 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +20,10 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('campus',EntityType::class, [
+                'class'=>Campus::class,
+                'choice_label'=>'nom',
+            ])
             ->add('username')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
@@ -43,6 +50,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ;
         ;
     }
 
