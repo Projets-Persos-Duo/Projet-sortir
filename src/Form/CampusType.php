0d7cdf041abd76fc44fb8e113ad6b960a551c73d
LULU <LULU@192.168.1.48>
2022-02-08 17:13:19 +0100

    correction typo Campus1Type->CampusType

--- src/Form/CampusType.php
+++ src/Form/CampusType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Campus;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +12,7 @@ class CampusType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom',TextType::class, ['label'=>'Nom du Campus'])
+            ->add('nom')
         ;
     }
 
