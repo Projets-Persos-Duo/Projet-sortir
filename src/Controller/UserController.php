3374ff55c3145178be243aeea97d36c1b6d9d93e
Meithal <hurin8888@gmail.com>
2022-02-10 20:28:22 +0100

    Feature: changement de MDP

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,9 +7,12 @@ use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 /**
  * @Route("/user")
@@ -61,24 +64,49 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="user_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(
+        Request $request,
+        User $user,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $userPasswordHasher
+    ): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
-        $selfUser = $this->getUser(); // @type
-        if(!$this->getUser()->getIsAdmin()) {
-            if($this->getUser()->getId() !== (int)$request->get('id')) {
+        $selfUser = $this->getUser();
+        if(!$selfUser->getIsAdmin()) {
+            if($selfUser->getId() !== (int)$request->get('id')) {
                 throw $this->createAccessDeniedException('Non autorisé');
             }
         };
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $oldMDP = $form->get('oldMDP')->getData();
+
+            if($oldMDP) {
+                $valid = $userPasswordHasher->isPasswordValid($user, $oldMDP);
+                if ($valid) {
+                    $user->setPassword(
+                        $userPasswordHasher->hashPassword(
+                            $user,
+                            $form->get('newMDP')->getData()
+                        )
+                    );
+                } else {
+                    $form->addError(new FormError('Mot de passe invalide !'));
+                    goto fail;
+                }
+            }
+
+            $entityManager->persist($user);
             $entityManager->flush();
+            $this->addFlash('success', 'Compte modifié !');
 
             return $this->redirectToRoute('user_show', ['id'=>$user->getId()], Response::HTTP_SEE_OTHER);
         }
 
+        fail:
         return $this->renderForm('user/edit.html.twig', [
             'user' => $user,
             'form' => $form,
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,13 +23,30 @@ class UserType extends AbstractType
 //            ->add('roles')
             ->add('email', EmailType::class)
             ->add('familyName', TextType::class, [
+                'required' => false,
                 'label' => 'Nom de famille'
             ])
             ->add('firstName', TextType::class, [
-                'label' => 'Prénom'
+                'label' => 'Prénom',
+                'required' => false,
             ])
             ->add('telephone', TelType::class, [
-                'label' => 'Téléphone'
+                'label' => 'Téléphone',
+                'required' => false
+            ])
+            ->add('oldMDP', PasswordType::class, [
+                'label' => 'Ancien mot de passe',
+                'required' => false,
+                'mapped' => false
+            ])
+            ->add('newMDP', RepeatedType::class, [
+                'first_name' => 'newMDP',
+                'second_name' => 'repeatMDP',
+                'first_options' => ['required' => false, 'label' => 'Nouveau mot de passe'],
+                'second_options' => ['required' => false, 'label' => 'Répéter mot de passe'],
+                'mapped' => false,
+                'required' => false,
+                'type' => PasswordType::class
             ])
 //            ->add('campus')
 //            ->add('groupes')
