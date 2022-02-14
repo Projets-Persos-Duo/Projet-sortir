81fa43092cfb3822e79f8340ba2e774604071bb6
Meithal <hurin8888@gmail.com>
2022-02-14 11:28:09 +0100

    tostring pour lieu

--- src/Entity/Lieu.php
+++ src/Entity/Lieu.php
@@ -60,6 +60,11 @@ class Lieu
         $this->sortiesRDV = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->nom;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
